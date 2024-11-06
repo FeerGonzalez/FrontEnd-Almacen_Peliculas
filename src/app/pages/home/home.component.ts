421346c455a5566dd491b74564dfc8071b249762
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2024-11-06 17:16:04 -0300

    avances

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,29 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { Pelicula } from '../../core/interfaces/pelicula';
 import { MoviesService } from '../../core/services/movies.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, CardModule, RouterModule],
+  imports: [ButtonModule, CardModule, RouterModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']  // Nota: Corregido styleUrl a styleUrls para que acepte un arreglo.
 })
-export class HomeComponent {
-  
-  movies:Pelicula[] = []
+export class HomeComponent implements OnInit {
 
-  constructor(private movieService: MoviesService){}
+  movies: Pelicula[] = [];
+
+  constructor(private movieService: MoviesService) {}
 
   ngOnInit(): void {
-
+    this.getAllMovies();
   }
 
-  getAllMovies(){
-    this.movieService.getMovies().subscribe(data=>{
-      this.movies = data;
-    })
+  getAllMovies(): void {
+    this.movieService.getMovies().subscribe({
+      next: (data) => this.movies = data,
+      error: (err) => console.error('Error al obtener las películas:', err)
+    });
+  }
+
+  deleteMovieById(id: number): void {
+    this.movieService.deleteMovieById(id).subscribe({
+      next: () => this.movies = this.movies.filter(movie => movie.id !== id),
+      error: (err) => console.error('Error al eliminar la película:', err)
+    });
+  }
+
+  trackById(index: number, movie: Pelicula): number {
+    return movie.id;
   }
 }
